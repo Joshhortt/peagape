d038d59cf6807d7f52b3c32b59a645747de215f3
Jose Horta <joshhortt@yahoo.com>
2019-11-24 16:11:32 +0000

    add Lesson 31 - insert into table

---
+++ 31insert_into.sql
@@ -0,0 +1,9 @@
+-- 31: MySQL Insert Into Database | PHP Tutorial | Learn PHP Programming | PHP for Beginners
+
+INSERT INTO posts (subject, content, date) VALUES('This ist the subject', 'Lorem ipsum dolor sit amet,
+ consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+ Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+   '2019-11-23 14:23:02');  
+    
