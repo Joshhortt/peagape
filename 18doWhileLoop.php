283d7909063c3bc25df77a16d1b27d986f9907f8
Jose Horta <joshhortt@yahoo.com>
2019-11-24 15:51:42 +0000

    add Lesson 18 - do while loop

---
+++ 18doWhileLoop.php
@@ -0,0 +1,34 @@
+
+<!--18: What Is a Do While Loop in PHP | PHP Tutorial | Learn PHP Programming | PHP for Beginners-->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Do While Loop</title>
+</head>
+<body>
+
+<?php
+
+ // There are 4 main Loops in PHP
+ // While Loop
+ // Do while Loop***
+ // For Loop
+ // Foreach Loop
+
+ $x = 2;
+ do {
+     // first it prints out the string then it looks after the while, in
+     // this case it echoes 4 times "Hi there!".
+    echo "<p class=red>Hi there!</p><br>";
+   $x++; 
+ }
+ while ($x <= 5); 
+ 
+?>
+
+</body>
+</html>
