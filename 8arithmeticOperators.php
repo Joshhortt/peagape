8017f07deb4808a52ea9aba0fb18d12334ff47d7
Jose Horta <joshhortt@yahoo.com>
2019-11-24 13:20:03 +0000

    add Lesson 8 - Arithmetic operators

---
+++ 8arithmeticOperators.php
@@ -0,0 +1,35 @@
+
+<!-- 8: What Are Arithmetic Operators in PHP
+
+Choose one of these online sources and read and practice
+
+Geeks For Geeks : https://www.geeksforgeeks.org/ 
+W3Schools : https://www.w3schools.com/ 
+PHP Documentation : https://www.php.net/
+
+-->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Arithmetic Operators</title>
+</head>
+<body>
+
+<?php
+
+echo 5+5;
+echo 5-5;
+echo 5*5;
+echo 5/5;
+echo 5%5; // percentage - divides to get left overs
+echo 5**5; // 5 to the power of 5
+
+?>
+
+</body>
+</html>
+
