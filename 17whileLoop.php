69d28b1b659e65ee4a6038a9430c02c6ffc012b7
Jose Horta <joshhortt@yahoo.com>
2019-11-24 15:50:39 +0000

    add Lesson 17 - while loop

---
+++ 17whileLoop.php
@@ -0,0 +1,35 @@
+
+<!--17: What Is a While Loop in PHP | PHP Tutorial | Learn PHP Programming | PHP for Beginners-->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  <title>While Loop</title>
+</head>
+<body>
+
+<?php
+
+ // There are 4 main Loops in PHP
+ // While Loop***
+ // Do while Loop
+ // For Loop
+ // Foreach Loop
+
+ 
+
+ $x = 1;
+      // 
+
+ while ($x <= 2) {
+   echo "<p class=red>Hi there!</p><br>";
+   $x++;
+ }
+
+?>
+
+</body>
+</html>
