f9cd981055af602152d0392dfc8434d314cc2e43
Jose Horta <joshhortt@yahoo.com>
2019-11-24 16:13:21 +0000

    add Lesson 32 - select from table

---
+++ 32select.sql
@@ -0,0 +1,13 @@
+-- 32: Select Data From MySQL Database | PHP Tutorial | Learn PHP Programming | PHP for Beginners
+
+-- examples using select 
+
+SELECT subject FROM posts  -- selecting only subject column from posts Table
+
+SELECT * FROM posts  -- selecting everything from posts table
+
+SELECT * FROM posts WHERE id='1' -- selecting everything from posts table where id=1
+
+SELECT * FROM posts WHERE id='1' AND subject='This is the subject' -- AND
+
+SELECT * FROM posts WHERE id='1' OR subject='This is the subject' -- OR
