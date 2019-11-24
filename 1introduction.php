031de2e92c986d7337926769306d2a105e90e0b6
Jose Horta <joshhortt@yahoo.com>
2019-11-24 11:44:22 +0000

    add Lesson 2 Installing local server Xampp

--- 1introduction.php
+++ 1introduction.php
@@ -1,4 +1,4 @@
-<!--1: Introduction to PHP Programming | PHP Tutorial | PHP For Beginners | Learn PHP Programming
+<!--1: Introduction to PHP Programming 
 
 Choose one of these online sources and read and practice
 
