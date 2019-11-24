65546aabff5ed1076fd0276decf8b888971173d8
Jose Horta <joshhortt@yahoo.com>
2019-11-24 11:46:06 +0000

    add Lesson 3 - output code in the browser

---
+++ 3outputBrowser.php
@@ -0,0 +1,32 @@
+<!-- 3: Output In Browser Using PHP  
+
+Choose one of these online sources and read and practice
+
+Geeks For Geeks : https://www.geeksforgeeks.org/ 
+W3Schools : https://www.w3schools.com/ 
+PHP Documentation : https://www.php.net/
+
+-->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Output PHP code in the Browser</title>
+</head>
+<body>
+
+<?php
+   # echo and print does the sam, the only diference is that echo is a tiny faster
+   echo "Hi there! ";
+   print "Jose ";
+   print 99; // integers do not need Quotation marks
+   echo 3+5; // also possible to use arithmetic operations.
+ 
+?>
+
+</body>
+</html>
+
