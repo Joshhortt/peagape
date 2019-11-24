5981ff04cb06975e121314571fe7f142d8f333f7
Jose Horta <joshhortt@yahoo.com>
2019-11-24 16:09:27 +0000

    add Lesson 29 - create mySql database

---
+++ 29mySqlDb.sql
@@ -0,0 +1,18 @@
+-- 29: How to Create a MySQL Database for PHP | PHP Tutorial | Learn PHP Programming | PHP Lesson
+
+/*FIRST PART
+1. Download Xampp
+2. Create Xampp shortcut for Desktop
+3. Open Xampp application
+4. Start Apache
+5. Start SQL
+6. Go to browser and type in: localhost/127.0.0.1 or localhost/phpmyadmin 
+
+SECOND PART
+We just use in the beginning the menu 'Database' and 'SQL'
+To create a Database we go to Databases or SQL
+Database need Tables to work - store information to one type of certain thing
+Don't use big letters and dashes. Use small letters and underscores
+*/
+
+CREATE DATABASE blog
