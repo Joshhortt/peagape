dccd0ac254f6c551a5fdf84f98d505ed7b998aee
Jose Horta <joshhortt@yahoo.com>
2019-11-24 16:16:01 +0000

    add Lesson 35 - order by

---
+++ 35orderBy.sql
@@ -0,0 +1,7 @@
+-- 35: Set Data Order From MySQL Database | PHP Tutorial | Learn PHP Programming | PHP for Beginners
+
+SELECT * FROM posts ORDER BY id ASC -- selecting all from posts table ordered by id ascending
+
+SELECT * FROM posts ORDER BY subject ASC  -- selecting all from posts table ordered by subject ascending
+
+SELECT * FROM posts ORDER BY subject DESC -- selecting all from posts table ordered by subject descending
