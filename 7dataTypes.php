a01d1c033b55b183efe9c7e7779329a9c190c672
Jose Horta <joshhortt@yahoo.com>
2019-11-24 13:13:59 +0000

    add Lesson 7 - Different Data Types

---
+++ 7dataTypes.php
@@ -0,0 +1,54 @@
+
+<!-- 7: Different Data Types in PHP
+
+Choose one of these online sources and read and practice
+
+Geeks For Geeks : https://www.geeksforgeeks.org/ 
+W3Schools : https://www.w3schools.com/ 
+PHP Documentation : https://www.php.net/
+
+-->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Different Data Types</title>
+</head>
+<body>
+
+
+
+<?php
+
+
+ // try to use sing quotes instead double quotes
+ // because some times you have double quotes inside double quotes
+ // to avoid to cancel them out
+ 
+ //string
+ $name = 'Coding is fun';
+
+ //integer
+ $name = 20;
+
+ //float 
+ $name= 20.500;
+
+ //Boolean
+ // return true or false statements
+ //true = 1
+ //false = 0
+
+ //array
+ $names = array('Jose', 'Marisa', 'Jimmy');
+ echo $names[0];
+
+
+?>
+
+</body>
+</html>
+
