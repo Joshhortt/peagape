542e6b2a40fee9cc8067d7dd724518573cac3cb7
Jose Horta <joshhortt@yahoo.com>
2019-11-24 15:52:32 +0000

    add Lesson 19 - for loop

---
+++ 19forLoop.php
@@ -0,0 +1,33 @@
+
+<!--19: How to Create a For Loop in PHP | PHP Tutorial | Learn PHP Programming | PHP for Beginners-->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  <title>For Loop</title>
+</head>
+<body>
+
+<?php
+
+ // There are 4 main Loops in PHP
+ // While Loop
+ // Do while Loop
+ // For Loop***
+ // Foreach Loop
+
+      // we know exactly how many times it prints out. 
+      // This case it loops two times 3 = one time + 1 time = 4 = 2 times.
+ for ($x = 3; $x <= 4; $x++) {
+    echo "<p class=red>Hi there!</p>";
+  
+ }
+  
+ 
+?>
+
+</body>
+</html>
