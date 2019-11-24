a2cf0da6d5ea3c14ae0019a2086877f39ace46c6
Jose Horta <joshhortt@yahoo.com>
2019-11-24 15:56:59 +0000

    add Lesson 21 - arrays

---
+++ 21arrays.php
@@ -0,0 +1,26 @@
+
+<!--21: Using Arrays in PHP to Store Data | PHP Tutorial | Learn PHP Programming | PHP for Beginners-->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Foreach Loop</title>
+</head>
+<body>
+
+<?php
+
+
+// An array (bunch of data combined) you can store a lot of data in single variable
+ 
+$array = array("Daniel", "Jane", "Jacob", "Maria", "John", "Elizabeth");
+
+echo $array[4];
+
+?>
+
+</body>
+</html>
