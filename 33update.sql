102cdb34a09afa637c2c3d3daa85fde0dc799f00
Jose Horta <joshhortt@yahoo.com>
2019-11-24 16:13:57 +0000

    add Lesson 33 - update table

---
+++ 33update.sql
@@ -0,0 +1,5 @@
+-- 33: Update Data From MySQL Database | PHP Tutorial | Learn PHP Programming | PHP for Beginners
+
+UPDATE posts
+SET subject="This is a just a test", content="This replaces Lorem ipsum"
+WHERE 
