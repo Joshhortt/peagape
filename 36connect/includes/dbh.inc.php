b919d82ac594cf8eb7ac7617c4e67bbc919cb50f
Jose Horta <joshhortt@yahoo.com>
2019-11-24 17:04:19 +0000

    add Lesson 36 - how connect to a database

---
+++ 36connect/includes/dbh.inc.php
@@ -0,0 +1,12 @@
+<!-- 36: How to connect to a database in PHP | PHP tutorial | Learn PHP programming  -->
+
+<!--First we need to get the 4 parameters to get the databes running
+
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName ="loginsystem";
+
+$conn ="mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)"; // important to write parameters by order
---
+++ 36connect/index.php
@@ -0,0 +1,19 @@
+<?php
+    include_once 'includes/dbh.inc.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Connect Database</title>
+</head>
+<body>
+
+
+      <p>At the beginning of the index page include_once and create dbh.inc.php file wit all the db parameters and connect '$conn' to the database!</p>
+
+</body>
+</html>
